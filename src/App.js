e56bb5ccc7eb7570013e82565d48b08d581669b5
Ryan <tjddnfkdls0@naver.com>
2024-09-23 16:25:43 +0900

    webpack 및 리액트 환경설정 완료

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import styled from 'styled-components'
+export default props => {
+    return (
+        <div>과제 시작</div>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: ''}
+      ],
+    }),
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    static: [
+      path.join(__dirname, 'dist'),
+      path.join(__dirname, 'public'),
+    ],
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+    client: {
+      overlay: false,
+    }
+  }
+};
