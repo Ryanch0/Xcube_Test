9cfff61b513f373b32af71d637e4a684174ea881
Ryan <tjddnfkdls0@naver.com>
2024-09-24 17:22:18 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import LoginForm from './atomic/layouts/LoginForm'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import DetailPage from './atomic/layouts/DetailPage'
 
 export default props => {
     return (
         <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-                <Route path='/Xcube_Test/' element={<LoginForm/>}/>
-                <Route path='/Xcube_Test/detail' element={<DetailPage/>}/>
-        </Routes>
+            <Routes>
+                <Route path="/" element={<Navigate to="/home" />} />
+                <Route path='/home' element={<LoginForm />} />
+                <Route path='/detail' element={<DetailPage />} />
+            </Routes>
         </BrowserRouter>
     )
 }
