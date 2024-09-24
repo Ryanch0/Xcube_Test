c244a95d72254f19aa4219341ced9ec6066294b6
Ryan <tjddnfkdls0@naver.com>
2024-09-24 15:21:27 +0900

    디테일 페이지 완료

--- src/atomic/layouts/DetailPage.js
+++ src/atomic/layouts/DetailPage.js
@@ -4,7 +4,8 @@ import SideBar from '../molecules/SideBar'
 import { MAIN_BG_COLOR } from '../../const/color'
 import ViewTool from '../molecules/ViewTool'
 import EditTool from '../molecules/EditTool'
-
+import sample from '../../assets/sample.png'
+import samplelist from '../../assets/samplelist.png'
 
 
 const ContainerWrap = styled.div`
@@ -19,7 +20,29 @@ const ToolBoxWrap = styled.div`
     top: 0;
     bottom: 0;
     right: 0;
-    padding: 16px 16px 16px 0;
+    padding: 16px 16px 0 0;
+`
+const SampleImage = styled.div`
+    width: 40%;
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    img{
+        width: 100%;
+        height: 100%;
+    }
+`
+const SampleList = styled.div`
+    width: 100%;
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    top: 135%;
+
+    img{
+        width: 70%;
+    }
 `
 
 export default props => {
@@ -27,6 +50,12 @@ export default props => {
     return (
         <ContainerWrap>
             <SideBar/>
+            <SampleImage>
+                <img src={sample} alt='sample'/>
+            <SampleList>
+                <img src={samplelist}/>
+            </SampleList>
+            </SampleImage>
             <ToolBoxWrap>
             <ViewTool/>
             <div style={{height : '16px'}}/>
