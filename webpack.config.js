867d132c0911d20b1aa418118b3d531023a7f547
Ryan <tjddnfkdls0@naver.com>
2024-09-24 17:40:24 +0900

    test

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/Xcube_Test/',
+    publicPath: '/',
   },
   mode: 'production',
 
